d3a522d1321e92f554f4fa49d13c43f1ecbad755
paulkokos <paulkokos@yahoo.gr>
2020-12-19 07:42:04 +0200

    Node callbacks and error Handling

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var rect = require('./Rectangle.js');
-
-var rect = {
-	perimeter: (x,y) => (2*(x+y)),
-	area:(x,y) => (x*y)
-};
+//
+// var rect = {
+// 	perimeter: (x,y) => (2*(x+y)),
+// 	area:(x,y) => (x*y)
+// };
 
 function solveRectangle(l,b) {
-	console.log("Soving for rectangle with l= "+ l + "and b="+b);
+	console.log("Solving for rectangle with l= "+ l + "and b="+b);
 
-
-	if ( l<=0 || b <=0 ) {
-		console.log("Rectangle dimentions should be greater that zero: l="+l + "and b= " +b);
-	}else {
-		console.log("The area of the rectangle is " + rect.area(l,b));
-		console.log("The perimeter of the rectangle is "+ rect.perimeter(l,b));
-	}
+	rect(l,b,(err,rectangle) => {
+		if (err) {
+			console.log("ERROR: ",err.message)
+		} else {
+			console.log("The area of the rectangle of dimensions l =" +l+" and b= "+b+" is " + rectangle.area());
+			console.log("The perimeter of the rectangle of dimensions l =" +l+" and b= "+b+" is " + rectangle.perimeter());
+		}
+	});
+	// if ( l<=0 || b <=0 ) {
+	// 	console.log("Rectangle dimensions should be greater that zero: l="+l + "and b= " +b);
+	// } else {
+	// 	console.log("The area of the rectangle is " + rect.area(l,b));
+	// 	console.log("The perimeter of the rectangle is "+ rect.perimeter(l,b));
+	// }
+	console.log("This statement is after the call to rect()")
 
 }
 solveRectangle(2,4);
